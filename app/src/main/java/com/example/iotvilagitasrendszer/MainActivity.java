6fcfadc1150eafd4e32cd0585593bb5246cc24c5
roland1997 <makszinka@gmail.com>
2020-04-08 20:23:45 +0300

    SeekBar with progressbar working, connected to firebase

--- app/src/main/java/com/example/iotvilagitasrendszer/MainActivity.java
+++ app/src/main/java/com/example/iotvilagitasrendszer/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.iotvilagitasrendszer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Lux;
+    private SeekBar Reference;
+    private ProgressBar progressBar;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialization();
+
+
+
+        Reference.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            progressBar.setProgress(progress);
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("Reference");
+
+
+                    myRef.setValue((progress*4095)/100);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    private void initialization (){
+
+        Lux =(TextView) findViewById(R.id.lux);
+        Reference = (SeekBar) findViewById(R.id.reference);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+    }
+
+
 }
