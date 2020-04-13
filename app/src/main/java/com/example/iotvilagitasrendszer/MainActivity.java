fef5ee7c79cb5f3c59af357d9824c402ab41aec4
roland1997 <makszinka@gmail.com>
2020-04-13 23:58:53 +0300

    RGB picker

--- app/src/main/java/com/example/iotvilagitasrendszer/MainActivity.java
+++ app/src/main/java/com/example/iotvilagitasrendszer/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.iotvilagitasrendszer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -20,8 +26,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView Lux;
     private SeekBar Reference;
     private ProgressBar progressBar;
+    private ImageView RGBColor;
+    View mColorView;
+    Bitmap bitmap;
     private DatabaseReference myRef;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +39,32 @@ public class MainActivity extends AppCompatActivity {
 
         initialization();
 
+        RGBColor.setDrawingCacheEnabled(true);
+        RGBColor.buildDrawingCache(true);
 
 
+        RGBColor.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE)
+                {
+                    bitmap = RGBColor.getDrawingCache();
+
+                    int pixel = bitmap.getPixel((int)event.getX(), (int)event.getY());
+
+                    //getting RGB values'
+                    int r = Color.red(pixel);
+                    int g = Color.green(pixel);
+                    int b = Color.blue(pixel);
+
+                    // set background color of view according to the picked color
+                   // mColorView.setBackgroundColor(Color.rgb(r,g,b));
+                    mColorView.setBackgroundColor(Color.rgb(r,g,b));
+                    Lux.setText("RGB: "+r +", "+ g +", "+ b);
+                }
+                return true;
+            }
+        });
         Reference.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -60,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
         Lux =(TextView) findViewById(R.id.lux);
         Reference = (SeekBar) findViewById(R.id.reference);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        RGBColor = (ImageView) findViewById(R.id.RGB_color);
+        mColorView = (View) findViewById(R.id.colorView);
 
     }
 
